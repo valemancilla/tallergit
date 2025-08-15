e208bcd83dcbab1cb45b3d8a534d0262b4ca804e
valemancilla <217262158+valemancilla@users.noreply.github.com>
2025-08-14 19:50:03 -0500

    feat: :sparkles: taller git
    se realiza el  ultimo taller con git flow

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    document.getElementById('formOutput').textContent = 'Nombre: ' + name + ', Email: ' + email;
+});
